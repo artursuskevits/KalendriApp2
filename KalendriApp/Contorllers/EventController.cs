a32a03e1e493f1bf8692710a8e48be57a657e2ca
artursuskevits <120181393+artursuskevits@users.noreply.github.com>
2024-10-30 05:47:30 +0200

    prefinal???

--- KalendriApp/Contorllers/EventController.cs
+++ KalendriApp/Contorllers/EventController.cs
@@ -5,6 +5,8 @@ using KalenderApp.Data;
 using KalenderApp.Models;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace KalenderApp.Controllers
 {
@@ -39,19 +41,24 @@ namespace KalenderApp.Controllers
 
         // POST: /Event/Create
         [HttpPost]
-        public async Task<IActionResult> Create(Event @event)
+        public async Task<IActionResult> Create(Event newEvent)
         {
             var userId = HttpContext.Session.GetInt32("UserId");
             if (userId == null) return RedirectToAction("Login", "Account");
 
-            @event.UserId = userId.Value;
-            _context.Add(@event);
+            // Setting user and default values if not filled
+            newEvent.UserId = userId.Value;
+            newEvent.Timezone ??= "Europe/Tallinn";
+            newEvent.Recurrence ??= "none";
+            newEvent.Reminder ??= "none";
+
+            _context.Add(newEvent);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
-        // GET: /Event/Edit/{id}
-        public async Task<IActionResult> Edit(int id)
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id, string lang = "et")
         {
             var userId = HttpContext.Session.GetInt32("UserId");
             if (userId == null) return RedirectToAction("Login", "Account");
@@ -62,43 +69,17 @@ namespace KalenderApp.Controllers
                 return NotFound();
             }
 
-            return View(@event);
-        }
-
-        // POST: /Event/Edit
-        [HttpPost]
-        public async Task<IActionResult> Edit(Event @event)
-        {
-            var userId = HttpContext.Session.GetInt32("UserId");
-            if (userId == null) return RedirectToAction("Login", "Account");
-
-            if (@event.UserId != userId)
-            {
-                return Unauthorized();
-            }
-
-            _context.Update(@event);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-
-        // GET: /Event/Delete/{id}
-        public async Task<IActionResult> Delete(int id)
-        {
-            var userId = HttpContext.Session.GetInt32("UserId");
-            if (userId == null) return RedirectToAction("Login", "Account");
-
-            var @event = await _context.Events.FindAsync(id);
-            if (@event == null || @event.UserId != userId)
-            {
-                return NotFound();
-            }
+            // Set language culture if needed
+            CultureInfo newCulture = new CultureInfo(lang);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+            ViewBag.Language = lang;
 
             return View(@event);
         }
 
         // POST: /Event/DeleteConfirmed
-        [HttpPost]
+        [HttpPost, ActionName("Delete")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var userId = HttpContext.Session.GetInt32("UserId");
@@ -112,6 +93,63 @@ namespace KalenderApp.Controllers
             }
 
             return RedirectToAction("Index");
+        }
+
+        // GET: /Event/Edit/{id}
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id, string lang = "et")
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null) return RedirectToAction("Login", "Account");
+
+            var @event = await _context.Events.FindAsync(id);
+            if (@event == null || @event.UserId != userId)
+            {
+                return NotFound();
+            }
+
+            // Set language culture if needed
+            CultureInfo newCulture = new CultureInfo(lang);
+            Thread.CurrentThread.CurrentCulture = newCulture;
+            Thread.CurrentThread.CurrentUICulture = newCulture;
+            ViewBag.Language = lang;
+
+            ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name");
+            return View(@event);
+        }
+
+        // POST: /Event/Edit
+        [HttpPost]
+        public async Task<IActionResult> Edit(Event @event)
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null) return RedirectToAction("Login", "Account");
+
+            if (@event.UserId != userId) return Unauthorized();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@event);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await _context.Events.AnyAsync(e => e.Id == @event.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Categories = new SelectList(await _context.Categories.ToListAsync(), "Id", "Name");
+            return View(@event);
         }
     }
 }
